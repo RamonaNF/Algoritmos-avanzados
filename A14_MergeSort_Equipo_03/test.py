cafaae5c4f8e3c297f9ecb0325343c3ea8f4aa91
armando <armando.rosas133@gmail.com>
2023-08-25 14:43:53 -0600

    update: mergesort

--- A14_MergeSort_Equipo_03/test.py
+++ A14_MergeSort_Equipo_03/test.py
@@ -7,14 +7,14 @@ print("\nEjecutando casos prueba...")
 
 for i in range(4): # Casos prueba aleatorios
     randomArray = list(np.random.rand(5)*100)
-    #randomArray2 = copy(randomArray)
+    randomArray2 = copy(randomArray)
     
     print("Array: ", randomArray, "\n")
 
-   # print("Primer método\n")
-   # sort1(randomArray)
-   # print("Array Ordenado", randomArray, "\n")
+    print("Primer método ", end="")
+    sort1(randomArray)
+    print(randomArray)
     
-    print("Segundo métodoo\n")
-    sort2(randomArray, 0, len(randomArray) - 1)
-    print("Array Ordenado", randomArray, "\n")
+    print("Segundo método ", end="")
+    sort2(randomArray2, 0, len(randomArray2) - 1)
+    print(randomArray2, "\n")
