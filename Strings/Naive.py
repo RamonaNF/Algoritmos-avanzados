acaf5f19e528e931eb7e3a8a3f16ed1c017325a1
RamonaNF <a01423596@tec.mx>
2023-09-14 12:53:06 -0600

    feat: algoritmo ingenuo

--- Strings/Naive.py
+++ Strings/Naive.py
@@ -6,3 +6,28 @@
                                   n: longitud de la cadena a buscar
   
 """
+from time import time
+
+def naive_matching(text: str, pattern: str):
+  iterations = 0
+  start_timer = time()
+  
+  coincidences = []
+  candidates = len(text) - len(pattern) + 1
+
+  for i in range(candidates):
+    coincidence = True
+
+    for j in range(len(pattern)):
+      if pattern[j] != text[i + j]:
+        coincidence = False
+        break
+
+      iterations += 1
+
+    if coincidence:
+      coincidences.append(i)
+
+  end_time = time() - start_timer
+
+  return iterations, coincidences, end_time
